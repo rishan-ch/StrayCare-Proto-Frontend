f78e1901c63e331d407a69567b5b923bafe77e8a
rishan-ch <rishanchhunju@gmail.com>
2024-10-06 16:58:07 +0545

    react route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
 import Login from './components/Login'
 import Register from './components/Register'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home'
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path:"/login",
+      element:<Login/>
+    },
+    {
+      path:"/register",
+      element:<Register/>
+    }
+  ])
+
   return (
-    <Login/>
+    <>
+    <RouterProvider router={router}/>
+    </>
+    
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,30 @@
 import axios from "axios";
 import { useState } from "react"
+import { Navigate, useNavigate } from "react-router-dom";
 export default function Login(){
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [userLogged, setUserLogged] = useState(true)
+    const navigate = useNavigate()
 
     async function handleSubmit(event){
         event.preventDefault();
+        try {
+            let res = await axios.post("http://localhost:8080/api/user/login",{email,password})
 
-        let res = await axios.post("http://localhost:8080/api/user/login",{email,password})
-
-        if(res.status===200){
-            console.log(res.data);
+            if(res.status===200){
+                console.log(res.data);
+                navigate("/")
+            }else{
+                // navigate("/login")
+                setUserLogged(false);
+            }
+        } catch (error) {
+            console.log('Login error:', error);
+            setUserLogged(false);
+            navigate('/login')
         }
-
     }
 
 
@@ -21,6 +32,7 @@ export default function Login(){
         <div>
             <div>
                 <form action="" onSubmit={handleSubmit}>
+
                     <div>
                         <label htmlFor="email">Email</label>
                         <input type="email" placeholder="Enter your email" onChange={e=> setEmail(e.target.value)}/>
@@ -30,6 +42,11 @@ export default function Login(){
                         <input type="password" placeholder="Enter your password" onChange={e=> setPassword(e.target.value)}/>
                     </div>
                     <button type="submit">Login</button>
+                    {
+                    !userLogged && (
+                        <p style={{color:'red'}}>The user credentials did not match.</p>
+                    )
+                    }
                 </form>
             </div>
         </div>
